c42f051b30afaca336b930590bc310f551b2ef01
RickeyRM <79210295+RickeyRM@users.noreply.github.com>
2021-03-20 00:09:47 +0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
-function check(element) {
-    const isChecked = element.classList.contains('form__product--bg');
-    console.log(isChecked);
+// function check(element) {
+//     const isChecked = element.classList.contains('form__product--bg');
+//     console.log(isChecked);
     
-    isChecked ? element.classList.remove('form__product--bg') : element.classList.add('form__product--bg')
-    }
+//     isChecked ? element.classList.remove('form__product--bg') : element.classList.add('form__product--bg')
+//     }
